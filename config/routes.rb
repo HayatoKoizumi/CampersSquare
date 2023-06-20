8b75fa49c4b0ebbd6c03913df92902afacfdb057
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-06-20 09:42:07 +0000

    [Update]public/homes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root to: 'homes#top'
-    get 'homes/about'
+    get 'about' => 'homes#about'
 
     resources :users, only: [:show, :edit, :update] do
       resource :relationships, only: [:create, :destroy]
