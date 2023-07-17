653e4d6ac807cb6c4c4c9ab6da686301c480f7a1
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-07-17 13:58:18 +0000

    [Update]最終調整

--- app/models/post_camp.rb
+++ app/models/post_camp.rb
@@ -13,8 +13,8 @@ class PostCamp < ApplicationRecord
   has_many :vision_tags, dependent: :destroy
 
   validates :image, presence: true
-  validates  :title, presence:  true, length: { maximum: 20 }
-  validates  :body, presence:  true, length: { maximum: 300 }
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 300 }
 
   def get_image(width, height)
     unless image.attached?
