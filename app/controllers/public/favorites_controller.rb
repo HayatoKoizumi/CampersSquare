743f9854df62820b46518dabc65758baaddab783
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-10-03 17:01:57 +0000

    [Update]いいね機能の非同期通信化 途中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,13 +4,13 @@ class Public::FavoritesController < ApplicationController
     post_camp = PostCamp.find(params[:post_camp_id])
     favorite = current_user.favorites.new(post_camp_id: post_camp.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
       post_camp = PostCamp.find(params[:post_camp_id])
       favorite = current_user.favorites.find_by(post_camp_id: post_camp.id)
       favorite.destroy
-      redirect_to request.referer
+      #redirect_to request.referer
   end
 end
